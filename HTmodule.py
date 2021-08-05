662a6d47a9466d4f5d0cc6c0421396608513142d
Soham Roy <sohamroycfc@gmail.com>
2021-08-05 11:55:02 +0530

    Module updated. Bugs fixed. Finger counter added

--- HTmodule.py
+++ HTmodule.py
@@ -12,7 +12,7 @@ class handDetect():
         self.mpHands = mp.solutions.hands
         self.hands = self.mpHands.Hands(self.mode,self.maxHands,self.detectionCon,self.trackCon)
         self.mpDraw = mp.solutions.drawing_utils
-
+        self.tipIds = [4, 8, 12, 16, 20]
     def findHand(self,img,draw=True):
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         self.results = self.hands.process(imgRGB)
@@ -26,18 +26,50 @@ class handDetect():
 
 
     def findPos(self,img,handNo=0,draw=True):
-        lmList=[]
+        self.lmList=[]
+        bbox=[]
+        xList=[]
+        yList=[]
         if self.results.multi_hand_landmarks:
             myHand=self.results.multi_hand_landmarks[handNo]
             for id, lm in enumerate(myHand.landmark):
                 #print(id, lm)
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
+                xList.append(cx),yList.append(cy)
+
                 #print(id, cx, cy)
-                lmList.append([id,cx,cy])
+                self.lmList.append([id,cx,cy])
                 if draw:
                     cv2.circle(img, (cx, cy), 10, (0,215,245), cv2.FILLED)
-        return lmList
+
+            xMin, xMax = min(xList), max(xList)
+            yMin, yMax = min(yList), max(yList)
+            bbox=xMin,yMin,xMax,yMax
+            if draw:
+                cv2.rectangle(img,(xMin-20,yMin-20),(xMax+20,yMax+20),(0,0,255),2)
+
+        return self.lmList,bbox
+
+    def fingersUp(self):
+        fingers = []
+        # Thumb
+        if self.lmList[self.tipIds[0]][1] > self.lmList[self.tipIds[0] - 1][1]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
+
+        # Fingers
+        for id in range(1, 5):
+
+            if self.lmList[self.tipIds[id]][2] < self.lmList[self.tipIds[id] - 2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+
+        #totalFingers = fingers.count(1)
+
+        return fingers
 
 def main():
     pTime = 0
@@ -47,7 +79,7 @@ def main():
     while True:
         success, img = cap.read()
         img=detector.findHand(img)
-        lmList=detector.findPos(img)
+        lmList,bbox=detector.findPos(img)
         if len(lmList)!=0:
             print(lmList[8])
         cTime = time.time()
@@ -58,10 +90,6 @@ def main():
 
         cv2.imshow("image", img)
         cv2.waitKey(1)
-
-
-
-
 
 
 if __name__=="__main__":
