e738f3ffcc282dcc42a21248590e1bf286c1d4aa
Soham Roy <sohamroycfc@gmail.com>
2021-08-08 04:41:35 +0530

    mouse value out of bounds sorted, finger distance measured for click

--- HTmodule.py
+++ HTmodule.py
@@ -1,6 +1,8 @@
 import cv2
 import mediapipe as mp
 import time
+import math
+import numpy as np
 
 
 class handDetect():
@@ -41,7 +43,7 @@ class handDetect():
                 #print(id, cx, cy)
                 self.lmList.append([id,cx,cy])
                 if draw:
-                    cv2.circle(img, (cx, cy), 10, (0,215,245), cv2.FILLED)
+                    cv2.circle(img, (cx, cy), 6, (0,215,245), cv2.FILLED)
 
             xMin, xMax = min(xList), max(xList)
             yMin, yMax = min(yList), max(yList)
@@ -70,6 +72,34 @@ class handDetect():
         #totalFingers = fingers.count(1)
 
         return fingers
+
+    def findDistance(self, p1, p2, img, draw=True, r=15, t=3):
+        x1, y1 = self.lmList[p1][1:]
+        x2, y2 = self.lmList[p2][1:]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+        if draw:
+            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), t)
+            cv2.circle(img, (x1, y1), r, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x2, y2), r, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (cx, cy), r, (0, 0, 255), cv2.FILLED)
+        length = math.hypot(x2 - x1, y2 - y1)
+
+        return length, img, [x1, y1, x2, y2, cx, cy]
+
+    def findDistance(self, p1, p2, img, draw=True, r=15, t=3):
+        x1, y1 = self.lmList[p1][1:]
+        x2, y2 = self.lmList[p2][1:]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+        if draw:
+            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), t)
+            cv2.circle(img, (x1, y1), r, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x2, y2), r, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (cx, cy), r, (0, 0, 255), cv2.FILLED)
+        length = math.hypot(x2 - x1, y2 - y1)
+
+        return length, img, [x1, y1, x2, y2, cx, cy]
 
 def main():
     pTime = 0
